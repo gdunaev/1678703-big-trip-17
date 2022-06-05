5f3182dc82b3dbc139112f778e9ea190476854c7
Gleb Dunaev~ <7-v-7@bk.ru~>
2022-06-05 12:41:20 +0300

    4. исправляет лишние проверки

--- src/utils/render.js
+++ src/utils/render.js
@@ -17,7 +17,7 @@ const replace = (newChild, oldChild) => {
   }
   const parent = oldChild.parentElement;
 
-  if (parent === null || oldChild === null || newChild === null) {
+  if (!parent || !newChild) {
     throw new Error('Can`t replace unexisting elements');
   }
 
